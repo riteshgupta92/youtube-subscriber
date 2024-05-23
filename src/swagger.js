3f0a2c157eb9a2d67adb5c09d249d07dcf958733
Vivek Pathak <vivekpathak8198@gmail.com>
2024-05-23 23:45:21 +0530

    swagger add

---
+++ src/swagger.js
@@ -0,0 +1,139 @@
+import swaggerJsdoc from "swagger-jsdoc";
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Get Youtube-Subscriber Api",
+      version: "1.0.0",
+      description:
+        "Documentation for the Get Youtube-Subscriber API built with ExpressJS",
+    },
+    servers: [
+      {
+        url: "localhost:8000/",
+        description: "Deployment server",
+      },
+    ],
+    components: {
+      schemas: {
+        Subscriber: {
+          type: "object",
+          properties: {
+            _id: {
+              type: "string",
+              description: "The unique identifier for the subscriber",
+            },
+            name: {
+              type: "string",
+              description: "The name of the subscriber",
+            },
+            subscribedChannel: {
+              type: "string",
+              description: "The channel to which the subscriber is subscribed",
+            },
+            subscribedDate: {
+              type: "string",
+              format: "date-time",
+              description: "The date when the subscriber subscribed",
+            },
+          },
+        },
+        SubscriberNameAndChannel: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+              description: "The name of the subscriber",
+            },
+            subscribedChannel: {
+              type: "string",
+              description: "The channel to which the subscriber is subscribed",
+            },
+          },
+        },
+      },
+    },
+    paths: {
+      "/subscribers": {
+        get: {
+          summary: "Get all subscribers",
+          description: "Returns a list of all subscribers.",
+          responses: {
+            200: {
+              description: "An array of subscribers.",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "array",
+                    items: {
+                      $ref: "#/components/schemas/Subscriber",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      "/subscribers/names": {
+        get: {
+          summary: "Get subscriber names and channels",
+          description:
+            "Retrieves names and subscribed channels of all subscribers without unique identifiers and subscription dates.",
+          responses: {
+            200: {
+              description: "An array of subscriber names and channels.",
+              content: {
+                "application/json": {
+                  schema: {
+                    type: "array",
+                    items: {
+                      $ref: "#/components/schemas/SubscriberNameAndChannel",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      "/subscribers/{id}": {
+        get: {
+          summary: "Get subscriber by ID",
+          description:
+            "Retrieves a single subscriber information by its unique identifier.",
+          parameters: [
+            {
+              in: "path",
+              name: "id",
+              required: true,
+              description: "Subscriber ID",
+              schema: {
+                type: "string",
+              },
+            },
+          ],
+          responses: {
+            200: {
+              description: "The requested subscriber",
+              content: {
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/Subscriber",
+                  },
+                },
+              },
+            },
+            404: {
+              description: "Subscriber not found.",
+            },
+          },
+        },
+      },
+    },
+  },
+  apis: ["./routes/*.js"],
+};
+
+export default options;
